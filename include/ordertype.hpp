f8b7b063bc5ab3fcc312afc539d54cf5ad100228
Y-Cyan-Zhang <yzhan687@jh.edu>
2026-06-23 20:00:11 -0400

    added code skeleton foor Order header file

--- include/ordertype.hpp
+++ include/ordertype.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+enum class OrderType {
+  GoodTillCancel,
+  FillOrKill,
+  ImmediateOrCancel,
+};
+
+enum class Side {
+  Buy,
+  Sell
+};
+
+
+class Order {
+public:
+private:
+};
