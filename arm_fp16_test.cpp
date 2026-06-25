2cc89288677bbe90aa77f6102b03f904af201cc4
Y-Cyan-Zhang <yzhan687@jh.edu>
2026-06-25 18:13:43 -0400

    changed if/else macro print statements

--- arm_fp16_test.cpp
+++ arm_fp16_test.cpp
@@ -10,13 +10,15 @@ void print_compile_time_support() {
   std::cout << "===1. Compile-Time Macros ===" << std::endl;
 #if defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
   std::cout << "__ARM_FEATURE_FP16_SCALAR_ARITHMETIC is defined." << std::endl;
-#endif
+#else
   std::cout << "__ARM_FEATURE_FP16_SCALAR_ARITHMETIC not supported." << std::endl;
+#endif
 
 #if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
-  std::cout << "__ARM_FEATURE_FP16_VECTOR_ARITHMETIC is supported." << std::endl;
-#endif
+  std::cout << "__ARM_FEATURE_FP16_VECTOR_ARITHMETIC is defined." << std::endl;
+#else
   std::cout << "__ARM_FEATURE_FP16_VECTOR_ARITHMETIC not supported." << std::endl;
+#endif
   
 }
 
