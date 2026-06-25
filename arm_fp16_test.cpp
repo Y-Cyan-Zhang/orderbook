e994b0f84b3c0a445abae59417b8b349018b0dd5
Y-Cyan-Zhang <yzhan687@jh.edu>
2026-06-25 18:12:32 -0400

    added test for fp16 vector arithmetic

--- arm_fp16_test.cpp
+++ arm_fp16_test.cpp
@@ -11,6 +11,13 @@ void print_compile_time_support() {
 #if defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
   std::cout << "__ARM_FEATURE_FP16_SCALAR_ARITHMETIC is defined." << std::endl;
 #endif
+  std::cout << "__ARM_FEATURE_FP16_SCALAR_ARITHMETIC not supported." << std::endl;
+
+#if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
+  std::cout << "__ARM_FEATURE_FP16_VECTOR_ARITHMETIC is supported." << std::endl;
+#endif
+  std::cout << "__ARM_FEATURE_FP16_VECTOR_ARITHMETIC not supported." << std::endl;
+  
 }
 
 
