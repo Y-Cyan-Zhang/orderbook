65bfe35acaeb1ac13a086f2649db2abe6da6e116
Y-Cyan-Zhang <yucyanzhang@gmail.com>
2026-07-02 12:32:41 +0800

    added file for type aliases and enums

---
+++ include/types.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+namespace orderbook {
+  using Price = std::int32_t;
+  using Quantity = std::int32_t;
+  using OrderId = std::uint64_t;
+
+  enum class OrderType {
+    GoodTillCancel,
+    ImmediateOrCancel,
+    FillOrKill,
+  };
+
+  enum class Side {
+    Buy,
+    Sell,
+  };
+}
