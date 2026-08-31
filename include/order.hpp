6fbc5556d53cd45b3843169c7f988fd0d860d437
Y-Cyan-Zhang <yucyanzhang@gmail.com>
2026-08-31 18:31:15 -0400

    started moving over some of the Order class's methods to the header file, mainly setters/getters

--- include/order.hpp
+++ include/order.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "types.hpp"
 namespace orderbook {
 
@@ -8,6 +9,16 @@ public:
     : orderType_{ orderType }, orderId_{ orderId }, side_{ side }, 
       price_{ price }, initialQuantity_{ quantity }, remainingQuantity_{ quantity }
   {}
+
+  OrderId getOrderId() const { return orderId_; }
+  OrderType getOrderType const { orderType_; }
+  Side getSide() const { return side_ };
+  Price getPrice() const { return price_ };
+  Quantity getInitialQuantity() const { return initialQuantity_; }
+  Quantity getRemainingQuantity() const { return remainingQuantity_; }
+  Quantity getFilledQuantity() const { return getInitialQuantity() - getRemainingQuantity(); }
+  bool isFilled() { return getRemainingQuantity() == 0; }
+  void fill(Quantity quantity); // TODO implementation in .cpp file
 
 private: 
   OrderType orderType_;
